501cb3429398feccdb7608e816670c2f81608bdd
scherrer <scherrer.bastien@gmail.com>
2017-02-07 16:10:01 +0100

    add list of Users participant to roadtrip with getter and setter

--- src/main/java/IUTGo/Models/RoadTrip.java
+++ src/main/java/IUTGo/Models/RoadTrip.java
@@ -1,5 +1,7 @@
 package IUTGo.Models;
 
+import IUTGo.Models.Users.Utilisateur;
+
 import java.io.*;
 import java.util.HashMap;
 
@@ -7,10 +9,12 @@ public class RoadTrip implements Serializable {
 
     private String nom;
     private HashMap<String, PointInteret> listePI;
+    private HashMap<String, Utilisateur> participants;
     private float price;
 
-    public RoadTrip(String nom) {
+    public RoadTrip(String nom, HashMap<String, Utilisateur> participants) {
         this.listePI = new HashMap<String, PointInteret>();
+        this.participants = participants;
         this.nom = nom;
     }
 
@@ -23,6 +27,7 @@ public class RoadTrip implements Serializable {
         tab = (HashMap<String, RoadTrip>) ooi.readObject();
         return tab;
     }
+
 
     public void addPI(PointInteret PI) {
         this.listePI.put(PI.getNom(), PI);
@@ -70,6 +75,14 @@ public class RoadTrip implements Serializable {
         return price;
     }
 
+    public HashMap<String, Utilisateur> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(HashMap<String, Utilisateur> participants) {
+        this.participants = participants;
+    }
+
     @Override
     public String toString() {
         return "RoadTrip{" +
@@ -78,4 +91,6 @@ public class RoadTrip implements Serializable {
                 ", prixMin Itinéraire=" + price +
                 '}';
     }
+
+
 }
