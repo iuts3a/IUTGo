78698e8ee9949e39411073144c2b9815cd840268
Antoine LERAT <antoine.lerat@etu.unice.fr>
2017-02-07 14:29:22 +0100

    Ajout classe conducteur passager et voyage du projet Tchatch_car

---
+++ src/main/java/IUTGo/Models/Conducteur.java
@@ -0,0 +1,15 @@
+package fr.unice.iut.info.methodo.s3a;
+
+import java.util.Date;
+
+/**
+ * Created by Silas on 18/10/2016.
+ */
+
+public class Conducteur extends Passager {
+
+    private Voyage ExporterVoyage(){
+        Voyage voyageTest = new Voyage();
+        return voyageTest;
+    }
+}
