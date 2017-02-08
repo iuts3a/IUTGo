8f78b622ffeb807ef0b808e7ad259195920f94fe
chloe <chloe.coupwarot@orange.fr>
2017-02-08 14:35:49 +0100

    add return to home page

--- src/main/java/IUTGo/Controllers/CreateItineraireController.java
+++ src/main/java/IUTGo/Controllers/CreateItineraireController.java
@@ -1,10 +1,16 @@
 package IUTGo.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by chloe on 08/02/2017.
@@ -32,7 +38,16 @@ public class CreateItineraireController {
 
     @FXML
     void homepage(ActionEvent event) {
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HomePageConnectedController.class.getClassLoader().getResource("home_page_connected.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) button_valider.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
     }
 
 }
