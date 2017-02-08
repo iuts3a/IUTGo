7490a064a86a9c0c52524a0ada97809fb9c29f61
chloe <chloe.coupwarot@orange.fr>
2017-02-08 15:17:10 +0100

    Add class

--- src/main/java/IUTGo/Controllers/OtherUserInfoController.java
+++ src/main/java/IUTGo/Controllers/OtherUserInfoController.java
@@ -3,5 +3,43 @@ package IUTGo.Controllers;
 /**
  * Created by chloe on 08/02/2017.
  */
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
 public class OtherUserInfoController {
+
+    @FXML
+    private Button btn_show_roadtrip;
+
+    @FXML
+    private Label lb_email;
+
+    @FXML
+    private ListView<?> tv_roadtrip;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private Label lb_lastname;
+
+    @FXML
+    private Label lb_telephone;
+
+    @FXML
+    private Label lb_name;
+
+    @FXML
+    void show_roadtrip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+
 }
