a926a6c6e0a60540ed44ec3b3b5ed9ff1a60ae0a
Victor Monsch <contact@victor-monsch.fr>
2017-02-08 13:39:19 +0100

    Modified controllers

---
+++ src/main/java/IUTGo/Controllers/UserInfoController.java
@@ -0,0 +1,64 @@
+package IUTGo.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class UserInfoController {
+
+    @FXML
+    private Button btn_show_roadtrip;
+
+    @FXML
+    private ListView<?> tv_notifications;
+
+    @FXML
+    private Button btn_create_rt;
+
+    @FXML
+    private ListView<?> tv_roadtrip;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private Button btn_accept_notif;
+
+    @FXML
+    private Button btn_delete_rt;
+
+    @FXML
+    private Button btn_refuse_modif;
+
+    @FXML
+    void create_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void show_roadtrip(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accept_notif(ActionEvent event) {
+
+    }
+
+    @FXML
+    void refuse_modif(ActionEvent event) {
+
+    }
+
+}
