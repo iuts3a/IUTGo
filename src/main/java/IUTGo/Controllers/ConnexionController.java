e88864627d6cc354973b72199e6f9c80c60839f1
VERAN <xavier.veran@etu.unice.fr>
2017-02-08 16:42:51 +0100

    Controllers (mis dans package IUTGo.Controllers+import) et MAJ fichiers FXML par conséquent

--- src/main/java/IUTGo/Controllers/ConnexionController.java
+++ src/main/java/IUTGo/Controllers/ConnexionController.java
@@ -27,6 +27,8 @@ public class ConnexionController {
 
     }
 
+
+
     @FXML
     void validate_connexion(ActionEvent event) {
 
---
+++ src/main/java/IUTGo/Controllers/InscriptionController.java
@@ -0,0 +1,70 @@
+package IUTGo.Controllers;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by xavier on 08/02/2017.
+ */
+
+public class InscriptionController {
+
+    @FXML
+    private Button valider;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private TextField mail;
+
+    @FXML
+    private Button retour;
+
+    @FXML
+    private PasswordField confirmPassword;
+
+    @FXML
+    private TextField nom;
+
+    @FXML
+    private TextField prenom;
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    private TextField numTel;
+
+    @FXML
+    private RadioButton accept;
+
+    @FXML
+    void homepage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valider(ActionEvent event) {
+
+    }
+
+    @FXML
+    void retour(ActionEvent event) {
+
+    }
+
+    @FXML
+    void accept(ActionEvent event) {
+
+    }
+
+    @FXML
+    void homepage(ActionEvent event) {
+
+    }
+
+}
