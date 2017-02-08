48ccf5b46616cd11f36c484b15a5dfa3d2417fbc
chloe <chloe.coupwarot@orange.fr>
2017-02-08 15:16:33 +0100

    Add return function

--- src/main/java/IUTGo/Controllers/UserInfoController.java
+++ src/main/java/IUTGo/Controllers/UserInfoController.java
@@ -2,8 +2,14 @@ package IUTGo.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by vmonsch on 08/02/2017.
@@ -37,6 +43,16 @@ public class UserInfoController {
 
     @FXML
     void create_rt(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(CreateItineraireController.class.getClassLoader().getResource("creation_itineraire.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) btn_accept_notif.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
 
     }
 
@@ -52,6 +68,16 @@ public class UserInfoController {
 
     @FXML
     void retour(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HomePageConnectedController.class.getClassLoader().getResource("home_page_connected.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) btn_accept_notif.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
 
     }
 
