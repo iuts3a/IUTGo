adb180a659d194340454e4af3271c7fb30eca56e
chloe <chloe.coupwarot@orange.fr>
2017-02-08 12:46:38 +0100

    Add skeleton

---
+++ src/main/java/IUTGo/Controllers/HomePageConnectedController.java
@@ -0,0 +1,52 @@
+package IUTGo.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Created by chloe on 08/02/2017.
+ */
+public class HomePageConnectedController {
+    @FXML
+    private Button btn_modify_rt;
+
+    @FXML
+    private Button btn_create_rt;
+
+    @FXML
+    private Button btn_search_pi;
+
+    @FXML
+    private Button btn_see_rt;
+
+    @FXML
+    private Label deconnecter;
+
+    @FXML
+    private Label informations;
+
+
+
+    @FXML
+    void create_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void see_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_pi(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modify_rt(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/IUTGo/Controllers/HomePageController.java
@@ -0,0 +1,43 @@
+package IUTGo.Controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+/**
+ * Created by chloe on 08/02/2017.
+ */
+public class HomePageController {
+    @FXML
+    private Label inscrire;
+
+    @FXML
+    private Button btn_create_rt;
+
+    @FXML
+    private Button btn_search_pi;
+
+    @FXML
+    private Label se_connecter;
+
+    @FXML
+    private Button btn_see_rt;
+
+
+    @FXML
+    void create_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void see_rt(ActionEvent event) {
+
+    }
+
+    @FXML
+    void search_pi(ActionEvent event) {
+
+    }
+}
+
