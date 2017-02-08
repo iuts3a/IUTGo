10b95cb501c69ab044ded1876dd79431e72ba2a7
chloe <chloe.coupwarot@orange.fr>
2017-02-08 16:52:16 +0100

    Add funtion return and validato

--- src/main/java/IUTGo/Controllers/ConnexionController.java
+++ src/main/java/IUTGo/Controllers/ConnexionController.java
@@ -1,8 +1,15 @@
 package IUTGo.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by xavier on 08/02/2017.
@@ -23,17 +30,54 @@ public class ConnexionController {
     private TextField username;
 
     @FXML
-    void redirect_register_view(ActionEvent event) {
+    private Label connection_error;
 
+    @FXML
+    public void  initialize() {
+        connection_error.setVisible(false);
+    }
+
+    @FXML
+    void redirect_register_view(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(InscriptionController.class.getClassLoader().getResource("inscription.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) button_register.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
     }
 
     @FXML
     void validate_connexion(ActionEvent event) {
+        connection_error.setVisible(false);
+        if((username.getText().trim().length()>0)&&(username.getText()!=null)&&(password.getText()!=null)){
+            try {
+                FXMLLoader fxmlLoader = new FXMLLoader(HomePageConnectedController.class.getClassLoader().getResource("home_page_connected.fxml"));
+                Parent root = fxmlLoader.load();
+                Stage stage = (Stage) button_register.getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.show();
+            } catch (IOException ex) {
+                System.err.println("Erreur au chargement: " + ex);
+            }
+        }
+        else{
+            connection_error.setVisible(true);
+            username.setText(null);
+            password.setText(null);
+        }
+
 
     }
 
     @FXML
     void homepage(ActionEvent event) {
+        connection_error.setVisible(true);
 
     }
 
