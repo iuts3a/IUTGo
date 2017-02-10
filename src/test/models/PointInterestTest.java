67fd8c859ebf47d48f3617bed9f6d8b2a4173131
alexandre <bolotalex06@gmail.com>
2017-02-10 10:42:20 +0100

    Mini correctif (après les renommages)...

--- src/test/models/PointInterestTest.java
+++ src/test/models/PointInterestTest.java
@@ -31,7 +31,8 @@ public class PointInterestTest
                                PointInterestType.MUSEUM,
                                100,
                                new Coordinates(1, 1, "Paris"),
-                               new User("Axel", "Mouchiroud", "@", "mdp", new Coordinates(1, 1, "Paris")));
+                               new User("Axel", "Mouchiroud", "@", "mdp",
+                                        new Coordinates(1, 1, "Paris")));
         
         
         //Methode nécessaire lors de modification de classe
