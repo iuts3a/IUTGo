4a429bf2fd2c17d73f0005d1cce8d4beecf957e6
scherrer <scherrer.bastien@gmail.com>
2017-02-10 13:38:12 +0100

    add connection enter FilterInteresetController and his view and a few method

--- src/main/java/IUTGo/Controllers/FilterInterestController.java
+++ src/main/java/IUTGo/Controllers/FilterInterestController.java
@@ -2,11 +2,17 @@ package IUTGo.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by vmonsch on 08/02/2017.
@@ -52,7 +58,16 @@ public class FilterInterestController {
 
     @FXML
     void homepage(ActionEvent event) {
-
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HomePageConnectedController.class.getClassLoader().getResource("home_page_connected.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) button_back.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
     }
 
     @FXML
