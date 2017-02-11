8a524d78dc34f15b2670a1954809fd6e4429aa15
scherrer <scherrer.bastien@gmail.com>
2017-02-11 17:56:35 +0100

    implement method in OtherUserInfoController

--- src/main/java/IUTGo/Controllers/OtherUserInfoController.java
+++ src/main/java/IUTGo/Controllers/OtherUserInfoController.java
@@ -1,16 +1,25 @@
 package IUTGo.Controllers;
 
-/**
- * Created by chloe on 08/02/2017.
- */
+import IUTGo.Models.RoadTrip;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class OtherUserInfoController {
 
+    private ObservableList data = FXCollections.observableArrayList();
     @FXML
     private Button btn_show_roadtrip;
 
@@ -18,7 +27,7 @@ public class OtherUserInfoController {
     private Label lb_email;
 
     @FXML
-    private ListView<?> tv_roadtrip;
+    private ListView<RoadTrip> tv_roadtrip;
 
     @FXML
     private Button retour;
@@ -34,12 +43,29 @@ public class OtherUserInfoController {
 
     @FXML
     void show_roadtrip(ActionEvent event) {
+        ArrayList<RoadTrip> listRoadTrip = new ArrayList<RoadTrip>((Collection<? extends RoadTrip>) tv_roadtrip);
+        for(RoadTrip roadTrip : listRoadTrip) {
+            data.add(roadTrip);
+        }
+        tv_roadtrip.setItems(data);
 
     }
 
     @FXML
     void retour(ActionEvent event) {
-
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(InscriptionController.class.getClassLoader().getResource("HomePage.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) retour.getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
 }
