1b47e143afa83fe80d04719cf667e5af9611dd77
chloe <chloe.coupwarot@orange.fr>
2017-02-12 19:13:54 +0100

    update FitrerInterestControler

--- src/main/java/IUTGo/Controllers/FilterInterestController.java
+++ src/main/java/IUTGo/Controllers/FilterInterestController.java
@@ -8,11 +8,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -27,28 +23,16 @@ import java.util.Map;
 public class FilterInterestController {
 
     @FXML
-    private TableColumn<?, ?> column_ville;
-
-    @FXML
-    private Button button_reset;
+    private TextField textfield_ville;
 
     @FXML
     private TextField textfield_nom;
 
     @FXML
-    private TableColumn<?, ?> column_nom;
+    private Button button_reset;
 
     @FXML
-    private TableColumn<?, ?> column_note;
-
-    @FXML
-    private TableView<?> tableview_PI;
-
-    @FXML
-    private ChoiceBox<?> choice_type;
-
-    @FXML
-    private TableColumn<?, ?> column_type;
+    private Button button_filtrer;
 
     @FXML
     private Button button_valider;
@@ -57,10 +41,10 @@ public class FilterInterestController {
     private Button button_back;
 
     @FXML
-    private Button button_filtrer;
+    private ListView<?> list_PI;
 
     @FXML
-    private TextField textfield_ville;
+    private TextField textfield_nom1;
 
     @FXML
     void homepage (MouseEvent event) {
@@ -80,6 +64,19 @@ public class FilterInterestController {
     void resetFiltres(ActionEvent event) {
         textfield_nom.setText(null);
         textfield_ville.setText(null);
+    }
+    @FXML
+    void goBack(ActionEvent event) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(HomePageConnectedController.class.getClassLoader().getResource("HomePageConnected.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = (Stage) button_back.getScene().getWindow();
+            Scene scene = new Scene(root, 1000, 510);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException ex) {
+            System.err.println("Erreur au chargement: " + ex);
+        }
     }
 
     @FXML
@@ -102,19 +99,13 @@ public class FilterInterestController {
 
             }
             list_PI.setItems(data);
-
-
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
-
-
-
     }
+
 
     @FXML
     void openPI(ActionEvent event) {
